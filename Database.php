1d1c6a3dfecc4b544c969f88d8b47aa0e04a1208
David Ong <lipweiong@gmail.com>
2022-10-03 23:56:14 +0800

    database

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$database = "foodlane";
+$username = "root";
+$password = "";
+
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+echo 'Connected successfully';
+
+mysqli_close($conn);
+
+?>
