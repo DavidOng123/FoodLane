fd81e5644df8db0c96d2e5688c3a9a661211f92c
Lee-ZC <leezc0631@gmail.com>
2022-10-16 22:48:21 +0800

    Complete the login flow, setting page, and some chart

---
+++ Admin/action.php
@@ -0,0 +1,91 @@
+<?php
+
+$connect = new PDO("mysql:host=localhost;dbname=foodlane", "root", "");
+
+
+if(isset($_POST["action"]))
+{
+	if($_POST["action"] == 'fetch')
+	{
+		$order_column = array('reserve_id', 'Pax', 'reserve_date_time');
+
+		$main_query = "
+		SELECT reserve_id, SUM(Pax) AS Pax, reserve_date_time 
+		FROM reservation 
+		";
+
+		$search_query = 'WHERE reserve_date_time <= "'.date('Y-m-d').'" AND ';
+
+                
+                if(isset($_POST["start_date"], $_POST["end_date"]) && $_POST["start_date"] != '' && $_POST["end_date"] != '')
+		{
+			$search_query .= 'reserve_date_time >= "'.$_POST["start_date"].'" AND reserve_date_time <= "'.$_POST["end_date"].'" AND ';
+		}
+                
+                
+		if(isset($_POST["search"]["value"]))
+		{
+			$search_query .= '(reserve_id LIKE "%'.$_POST["search"]["value"].'%" OR Pax LIKE "%'.$_POST["search"]["value"].'%" OR reserve_date_time LIKE "%'.$_POST["search"]["value"].'%")';
+		}
+
+		$group_by_query = " GROUP BY reserve_date_time ";
+
+		$order_by_query = "";
+
+		if(isset($_POST["order"]))
+		{
+			$order_by_query = 'ORDER BY '.$order_column[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' ';
+		}
+		else
+		{
+			$order_by_query = 'ORDER BY reserve_date_time DESC ';
+		}
+
+		$limit_query = '';
+
+		if($_POST["length"] != -1)
+		{
+			$limit_query = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+		}
+
+		$statement = $connect->prepare($main_query . $search_query . $group_by_query . $order_by_query);
+
+		$statement->execute();
+
+		$filtered_rows = $statement->rowCount();
+
+		$statement = $connect->prepare($main_query . $group_by_query);
+
+		$statement->execute();
+
+		$total_rows = $statement->rowCount();
+
+		$result = $connect->query($main_query . $search_query . $group_by_query . $order_by_query . $limit_query, PDO::FETCH_ASSOC);
+
+		$data = array();
+
+		foreach($result as $row)
+		{
+			$sub_array = array();
+
+			$sub_array[] = $row['reserve_id'];
+
+			$sub_array[] = $row['Pax'];
+
+			$sub_array[] = $row['reserve_date_time'];
+
+			$data[] = $sub_array;
+		}
+
+		$output = array(
+			"draw"			=>	intval($_POST["draw"]),
+			"recordsTotal"	=>	$total_rows,
+			"recordsFiltered" => $filtered_rows,
+			"data"			=>	$data
+		);
+
+		echo json_encode($output);
+	}
+}
+
+?>
--- Admin/login.php
+++ Admin/login.php
@@ -1,34 +1,151 @@
+<?php 
+session_start();
+
+    include("server.php");
+    
+	
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+            //something was posted
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            
+
+            if(!empty($username) && !empty($password) && !is_numeric($username))
+            {
+
+                //read from database
+                $query = "select * from customer where CustName = '$username' limit 1";
+                $result = mysqli_query($con, $query);
+
+                if($result)
+                {
+                    if($result && mysqli_num_rows($result) > 0)
+                    {
+                        $user_data = mysqli_fetch_assoc($result);
+
+                        if($user_data['password'] === $password)
+                        {
+                            
+                            //Validate user type
+                            if ($user_data['usertype']=="admin"){
+                                
+                            $_SESSION['username'] =  $username;
+                            
+                            header("Location: index.php");
+                            
+                            }
+                            
+                            else{
+                                 $_SESSION['StatusInvalid'] = 'Invalid Username OR Password !';
+                            }
+
+          
+                        }
+                    }
+                    
+                }
+
+                    $_SESSION['StatusInvalid'] = 'Invalid Username OR Password !';
+            }else
+            {
+                    $_SESSION['StatusInvalid'] = 'Invalid Username OR Password !';
+            }
+	}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
+
+    <head>
 	
-         <meta charset="utf-8" />
+        <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<title>Admin Login </title>
 	<link rel="stylesheet" href="css/admin_login_style.css">
-</head>
-<body>
-	<div class="box">
-		<form autocomplete="off">
-			<h2>Admin Sign In</h2>
-			<div class="inputBox">
-				<input type="text" required="required">
-				<span>Userame</span>
-				<i></i>
-			</div>
-			<div class="inputBox">
-				<input type="password" required="required">
-				<span>Password</span>
-				<i></i>
-			</div>
-			<div class="links">
-				<a href="#" style="color: transparent; ">Forgot Password ?</a>
-				<a href="../index.php">Back To Home</a>
-			</div>
-			<input type="submit" value="Login">
-                        <a href="index.php"> login </a>
-		</form>
-	</div>
-</body>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </head>
+
+    
+    
+    <body>
+        
+    <?php 
+
+    if(isset($_SESSION['StatusInvalid'])){
+        ?>
+        <script>
+        Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Username OR Password incorrect!',
+        footer: '<a href="../customerService.php">Why do I have this issue?</a>'
+        })
+        </script>
+
+        
+   
+   <?php
+        unset($_SESSION['StatusInvalid']);
+            
+    }
+    ?>    
+        
+    
+        <div class="box">
+                <form autocomplete="off" class="form col" method="post">
+                        <h2>Admin Sign In</h2>
+                        <div class="inputBox">
+                                <input type="text" required="required" name="username" id="username">
+                                <span>Username</span>
+                                <i></i>
+                        </div>
+                        <div class="inputBox">
+                                <input type="password" required="required" minlength="8" name="password" id="password">
+                                <span>Password</span>
+                                <i></i>
+                        </div>
+                        <div class="links">
+                                <a href="#" style="color: transparent; ">Forgot Password ?</a>
+                                <a href="../index.php">Back To Home</a>
+                        </div>
+                        <input type="submit" value="Login" class="btn">
+                       
+                </form>
+        </div>
+
+
+
+        <script>
+            const btn = document.querySelector(".btn");
+            const formEl = document.querySelector(".form");
+
+            var position;
+
+            // mouse hover event listener
+            btn.addEventListener("mouseover", function () {
+              //form validation callback return true or false, trigged because of built in form validation in HTML 'required'
+              if (!formEl.checkValidity()) {
+                position ? (position = 0) : (position = 200);
+                //adding the css animation property
+                btn.style.transform = `translate(${position}px, 0px)`;
+                btn.style.transition = "all 0.3s ease";
+              } else {
+                return;
+              }
+            });
+
+            //on click function
+            btn.addEventListener("click", function () {
+              e.preventDefault();
+              alert("welldone");
+            });                
+        </script>
+
+
+
+    </body>
 </html>
